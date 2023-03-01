29bb3f1749b024f1c0ae3c8d65470e774d497ca6
ahmead <ahmead1999.me@gmail.com>
2023-03-01 01:58:33 -0800

    doneeee

--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -11,13 +11,22 @@
   <table     class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-              
+            <th scope="col" class="py-3 px-6">
+                Order ID
+            </th>
               <th scope="col" class="py-3 px-6">
-                  Order ID
+                Customer Name
               </th>
               <th scope="col" class="py-3 px-6">
-                  Order Name
+                reciver Name              </th>
+                <th scope="col" class="py-3 px-6">
+                  order Name
+               </th>
+               <th scope="col" class="py-3 px-6">
+                serial Number
+             
               </th>
+
               <th scope="col" class="py-3 px-6">
                   Status
               </th>
@@ -27,21 +36,39 @@
       <tbody v-for="order in orders" :key="order">
           <tr  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
              
-              <th scope="row" class="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
-                  <div class="pl-3">
-                      <div class="text-base font-semibold" 
-                     
-                        >{{order.id}}</div>
-                  </div>  
-              </th>
+             
+            <th scope="row" class="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
+              <div class="pl-3">
+                  <div class="text-base font-semibold" 
+                 
+                    >{{order.orderId}}</div>
+              </div>  
+          </th>
+
+              
               <td class="py-4 px-6" >
-                {{order.Ordername}}
+                {{order.senderCustomerName}}
               </td>
               <td class="py-4 px-6">
                   <div class="flex items-center">
-                    {{order.status[1]}}
+                    {{order.reciverCustomerName}}
                   </div>
               </td>
+              <td class="py-4 px-6">
+                <div class="flex items-center">
+                  {{ order.orderItems.name }}
+                </div>
+            </td>
+            <td class="py-4 px-6">
+              <div class="flex items-center">
+                {{order.serialNumber}}
+              </div>
+          </td>
+          <td class="py-4 px-6">
+            <div class="flex items-center">
+              {{order.orderStauts}}
+            </div>
+        </td>
              
           </tr>
           
@@ -68,55 +95,22 @@ data () {
 
   return {
 
-    orders : [
-
-            {   id : '54574' , 
-                Ordername: 'Refrigerator',
-                status: ['Shipped', 'Picked Up' , ' Deliverd ' , ' In WhereHouse '],
-                
-            },
-
-            {   id : '54098' , 
-                Ordername: 'Refrigerator',
-                status: ['Shipped', 'Picked Up' , ' Deliverd ' , ' In WhereHouse '],
-                
-            },
-
-
-            {   id : '4345845' , 
-                Ordername: 'Refrigerator',
-                status: ['Shipped', 'Picked Up' , ' Deliverd ' , ' In WhereHouse '],
-                
-            },
-
-            {   id : '45865' , 
-                Ordername: 'vncxjvncxnvcx',
-                status: ['Shipped', 'Picked Up' , ' Deliverd ' , ' In WhereHouse '],
-               
-            },
-         
-
-        
-           
-           
-
-
-        ]
+    orders : []
   }
-}
-//created () {
-     // this.axios.get('https://v1.nocodeapi.com/ahmed000/ow/yEsnpsheKBYtzvrI/byCityName?q=khartoum')
-      //.then ((response =>{
-        //this.orders = response.data
-        //console.log(response.data);
-    //  } ) )
-      //.catch((error) => {
-        //console.log(error);
-     // })
+} ,
+created () {
+     this.axios.get('http://mowafaqwali-001-site1.gtempurl.com/api/Orders')
+      .then ((response =>{
+        this.orders = response.data
+      console.log(response.data);
+      } ) )
+      .catch((error) => {
+        console.log(error);
+     })
       
       
-  //}, 
+  }, 
 
-}
+}  
  
 </script>
